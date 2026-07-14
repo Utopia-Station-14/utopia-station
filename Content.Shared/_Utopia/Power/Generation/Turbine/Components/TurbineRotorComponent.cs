82204492155f855d56ec81dc6e9f87be6203a2bb
M1and1B <153716654+M1and1B@users.noreply.github.com>
2026-07-14 22:44:09 +0300

    fixxx

--- Content.Shared/_Utopia/Power/Generation/Turbine/Components/TurbineRotorComponent.cs
+++ Content.Shared/_Utopia/Power/Generation/Turbine/Components/TurbineRotorComponent.cs
@@ -27,11 +27,6 @@ namespace Content.Shared.Power.Turbines.Components
         [DataField] 
         public float MinPressure = 500f;
 
-        [DataField] 
-        public float CurrentPressure;
-        
-        [DataField] 
-        public float MaxPressure = 16101f;
         /// <summary>
         /// Модификатор оборотов в минуту. Чем меньше значение - тем больше оборотов в минуту будет делать турбина.
         /// </summary>
